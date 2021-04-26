a6f406e1f094d8d73fa89bc9957bb527a323d652
Kleiton De Souza Rocha <kleitonkltn@gmail.com>
2021-04-26 14:40:29 -0400

    design main story board and add function in button gerarNumero for generate random number of click action

--- sorterApp/ViewController.swift
+++ sorterApp/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelNumero: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func gerarNumero(_ sender: Any) {
+        var numero = arc4random_uniform(11)
+        labelNumero.text = String(numero)
+    }
+    
 }
 
